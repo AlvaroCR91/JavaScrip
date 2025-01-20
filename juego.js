599b8b22d534e1f3e8f3cddc7b871ced91ed70e0
Alvaro <acalvo91@hotmail.com>
2025-01-20 19:46:56 +0100

    arreglo

--- juego.js
+++ juego.js
@@ -1,11 +1,7 @@
 let imagenes = ["img/delegado1.jpg", "img/delegado2.jpg", "img/delegado3.jpg"];
 
 let tiempoSlot1=100;
-let tiempoSlot2=200;
-let tiempoSlot3=300;
 let manejarSlot1;
-let manejarSlot2;
-let manejarSlot3;
 
 function cambiarImagen() {
     let aleatorio = parseInt(Math.random() * imagenes.length);
@@ -24,12 +20,6 @@ function cambiarImagen() {
 function tirada() {
     manejarSlot1 = setInterval(cambiarImagen, tiempoSlot1);
     setInterval(pararSlot1, 3000);
-
-    manejarSlot2 = setInterval(cambiarImagen, tiempoSlot2);
-    setInterval(pararSlot2, 3000);
-
-    manejarSlot3 = setInterval(cambiarImagen, tiempoSlot3);
-    setInterval(pararSlot3, 3000);
 }
 
 function reducirtiempo(){
@@ -37,28 +27,10 @@ function reducirtiempo(){
     clearInterval(manejarSlot1);
     manejarSlot1 = setInterval(cambiarImagen, tiempoSlot1);
     setTimeout(pararSlot1, 2000);
-
-    tiempoSlot2 += 500;
-    clearInterval(manejarSlot2);
-    manejarSlot2 = setInterval(cambiarImagen, tiempoSlot2);
-    setTimeout(pararSlot2, 2000);
-
-    tiempoSlot3 += 500;
-    clearInterval(manejarSlot3);
-    manejarSlot3 = setInterval(cambiarImagen, tiempoSlot3);
-    setTimeout(pararSlot3, 2000);
 }
 
 function pararSlot1() {
     clearInterval(manejarSlot1);
 }
-function pararSlot2() {
-    clearInterval(manejarSlot2);
-}
-function pararSlot3() {
-    clearInterval(manejarSlot3);
-}
 
 tirada();
-
-setInterval(cambiarImagen, 500);
